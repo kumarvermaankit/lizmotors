08f73e1b5a557e6dbcdaa8b7df743d86fb59f583
unknown <kumarvermaankit9555@gmail.com>
2022-05-08 07:24:38 +0530

    update-front

--- client/src/gps.js
+++ client/src/gps.js
@@ -13,13 +13,108 @@ function App() {
   const [time, settime] = useState("00:00:00")
   const [speed, setspeed] = useState("0 KMPH")
   const [altitude, setaltitude] = useState("0 m")
-  const [interval, setinterval] = useState(500);
+  const [interval, setinterval] = useState(5000);
   const [stations,setstations] = useState({
     from:"",
     to:""
   })
+
+  const [token,settoken] = useState(0)
+
+  const [mapstate,setmapstate] = useState(false)
+
   useEffect(async () => {
 
+    //Fetching data from backend
+    // const excelData = [await axios.get("https://locationtracker9.herokuapp.com/api")]
+    if(mapstate === true){
+
+    
+    const excel = await axios.get(`http://localhost:5000/path/${stations.from}/${stations.to}`)
+    var excelData = []
+
+    Object.keys(excel.data).map(function(key, index) {
+      excelData.push(excel.data[key])
+    });
+    
+    console.log(excelData)
+    // Creating an instance of mapbox
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGFya3NpZGVyNTEiLCJhIjoiY2wwZHl6NXY4MGR2NzNjbXo5ZTBtZGE4eSJ9._xuts111DHQXPl5CflCMiQ';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      // style: 'mapbox://styles/darksider51/cl0h230xc001j14qk893hg13b',
+         style:'mapbox://styles/darksider51/cl2vx7rga000614nzlczzv91w',
+      zoom: 14
+    });
+
+
+
+    map.on('load', async () => {
+      // We fetch the JSON here so that we can parse and use it separately
+      // from GL JS's use in the added source.
+      const response = await fetch(
+        'https://docs.mapbox.com/mapbox-gl-js/assets/hike.geojson'
+      );
+      const data = await response.json();
+      // save full coordinate list for later
+      const coordinates = data.features[0].geometry.coordinates;
+
+      // start by showing just the first coordinate
+      // data.features[0].geometry.coordinates = [[excelData.data[0][3], excelData.data[8][2]]];
+      console.log(excelData)
+      data.features[0].geometry.coordinates = [[excelData[0].longitude, excelData[0].latitude]];
+
+      // add it to the map
+      map.addSource('trace', { type: 'geojson', data: data });
+      map.addLayer({
+        'id': 'trace',
+        'type': 'line',
+        'source': 'trace',
+        'paint': {
+          'line-color': 'red',
+          'line-opacity':1,
+          'line-width': 4,
+          
+        }
+      });
+
+      // setup the viewport
+
+
+      map.jumpTo({ 'center': [excelData[0].longitude, excelData[0].latitude], 'zoom': 14});
+      map.setPitch(30);
+
+      // on a regular basis, add more coordinates from the saved list and update the map
+      let i = 1;
+      const timer2 = setInterval(() => {
+        // console.log(excelData[i].latitude,excelData[i].longitude)
+        if (i<excelData.length) {
+          settoken(i)
+          // settime(excelData.data[i][1])
+          // setspeed(excelData.data[i][5])
+          // setaltitude(excelData.data[i][4])
+          // if (i > 500) {
+            map.jumpTo({ 'center': [excelData[i].longitude, excelData[i].latitude], 'zoom': 14});
+
+      
+            // }
+          // if (i === excelData.data.length - 1) {
+          //   map.jumpTo({ 'zoom': 10.75 });
+
+          // }
+
+          data.features[0].geometry.coordinates.push([excelData[i].longitude, excelData[i].latitude]);
+          map.getSource('trace').setData(data);
+          map.panTo([excelData[i].longitude, excelData[i].latitude]);
+          i++;
+        } else {
+          window.clearInterval(timer2);
+        }
+      }, interval);
+
+    });
+  }
+  else{
     //Fetching data from backend
     const excelData = await axios.get("https://locationtracker9.herokuapp.com/api")
     // const excelData = await axios.get(`http://localhost:5000/path/${stations.from}/${stations.to}`)
@@ -33,6 +128,9 @@ function App() {
       zoom: 14
     });
 
+
+
+    
 
 
     map.on('load', async () => {
@@ -95,7 +193,8 @@ function App() {
       }, interval);
 
     });
-  }, [interval])
+  }
+  }, [interval,mapstate])
 
 
 
@@ -105,8 +204,9 @@ function App() {
 
 
 async function getRoute(){
-  const a = await axios.get(`http://localhost:5000/path/${stations.from}/${stations.to}`)
-  console.log(a)
+  // const a = await axios.get(`http://localhost:5000/path/${stations.from}/${stations.to}`)
+  setmapstate(true)
+  // console.log(a)
 }
 
 
@@ -146,7 +246,9 @@ async function getRoute(){
 
         </select>
       </div>
-
+<div>
+  <h3>Tokens:{token}</h3>
+</div>
       <div style={{ display: "flex" }}>
         <div id="map"></div>
       <div>
@@ -156,6 +258,7 @@ async function getRoute(){
       <input onChange={(event)=>addStation(event)} name="to" />
       <button onClick={getRoute}>Submit</button>
       </div>
+
         {/* <div className='attributes'>
 
           <div id="attribute">
